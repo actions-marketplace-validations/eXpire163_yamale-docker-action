b6922f9cf5cf666a6a0d2a0056a8ff819562bcd7
eXpire163 <github@mms2k.de>
2021-03-06 23:20:35 +0100

    strict or non strict flag added

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh -l
-set -e
+#set -e
 
-echo "Hello $1"
-echo python --version
+echo $3
+
+if [ $3 = "strict" ] ; then
+   strict=" "
+   echo "using strict mode "
+else
+    strict=" --no-strict"
+    echo "using non strict mode "
+fi
+
+
+
 yamale --schema $1 $2
 time=$(date)
 echo "::set-output name=time::$time"
